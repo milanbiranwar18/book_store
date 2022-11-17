282db579a4d2ba259f43133bf313d76982650a68
Milan Biranwar <milanbiranwar06@gmail.com>
2022-11-17 11:29:07 +0530

    [Milan} Added class based view with serialization for user login and registration

--- user/serializer.py
+++ user/serializer.py
@@ -1,7 +1,15 @@
+import logging
+
 from django.contrib.auth import authenticate
 from rest_framework import serializers
+from rest_framework.response import Response
 
 from user.models import User
+
+logging.basicConfig(filename="serializer.log",
+                    filemode='a',
+                    format='%(asctime)s %(levelname)s-%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
@@ -10,7 +18,14 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ['id', 'first_name', 'last_name', 'username', 'password', 'email', 'mob_number', 'location']
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        """
+        Create and return a new `User` instance, given the validated data.
+        """
+        try:
+            return User.objects.create_user(**validated_data)
+        except Exception as e:
+            logging.error(e)
+            return Response({"message": str(e)})
 
 
 class LoginSerializer(serializers.Serializer):
@@ -18,4 +33,7 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(max_length=100)
 
     def create(self, validated_data):
-        return authenticate(**validated_data)
+        user = authenticate(**validated_data)
+        if not user:
+            raise Exception('Invalid credentials')
+        return user
